57c62df3fcea36ff60da6ac07481eea0bec39724
Mohammad Al-Rifaii <mohammad.alrifaii@tuum.com>
2025-12-02 01:27:24 +0200

    finish solving d1p2 and cleanup d1p1

--- 2025/d1p1.c
+++ 2025/d1p1.c
@@ -7,25 +7,23 @@ int main(void) {
         return 1;
     }
 
-    char line_buffer[6]; //big enough for any line
+    char line_buffer[6];
+    const char subtraction_sign = 'L';
     int dial = 50;
     int zero_count = 0;
     while (fgets(line_buffer, sizeof line_buffer, file) != NULL) {
         char sign;
         int number;
         sscanf(line_buffer, "%c%d", &sign, &number);
-        if (sign == 'L') {
-            dial -= number;
-        } else {
-            dial += number;
-        }
 
-        if (dial % 100 == 0) {
+        number = (sign == subtraction_sign ? number * -1 : number);
+        dial += number;
+
+        if (dial % 100 == 0)
             zero_count++;
-        }
     }
 
-    printf("%d\n", zero_count);
+    printf("%d\n", zero_count); //1150
 
     return 0;
 }
--- 2025/d1p2.c
+++ 2025/d1p2.c
@@ -1,6 +1,5 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<math.h>
 
 int main(void) {
     FILE *file = fopen("./input/d1-input.txt", "r");
@@ -9,7 +8,7 @@ int main(void) {
         return 1;
     }
 
-    char line_buffer[6];
+    char line_buffer[8];
     const char subtraction_sign = 'L';
     int dial = 50;
     int zero_count = 0;
@@ -19,22 +18,20 @@ int main(void) {
         int number;
         sscanf(line_buffer, "%c%d", &sign, &number);
 
+        number = (sign == subtraction_sign ? number * -1 : number);
         previous_dial = dial;
-
-        dial = (sign == subtraction_sign ? dial - number : dial + number);
-
-        if (dial == 0 || (previous_dial > 0 && dial < 0) || (previous_dial < 0 && dial > 0))
-            zero_count += 1;
+        dial += number;
 
         int remainder = dial % 100;
-        int to_add = (number - remainder) / 100;
+        zero_count+= abs(dial / 100);
 
-        zero_count += to_add;
+        if (dial == 0 || ((previous_dial > 0 && dial < 0) || (previous_dial < 0 && dial > 0)))
+            zero_count += 1;
+
         dial = remainder;
-        printf("%d (%c%d) %d, %d\n", previous_dial, sign, number, dial, zero_count);
     }
 
-    printf("%d\n", zero_count);
+    printf("%d\n", zero_count); //6738
 
     return 0;
 }
